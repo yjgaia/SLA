841990f3dd2d7160ffca8b3217bc8991028f0f61
oksktank <oksktank@gmail.com>
2012-10-29 14:24:51 +0900

    jedis 추가

---
+++ src/main/java/RedisTest.java
@@ -0,0 +1,17 @@
+import redis.clients.jedis.Jedis;
+
+
+
+public class RedisTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jedis jedis=new Jedis("1.234.80.226");
+		jedis.auth("ekfrrhrl0");
+		System.out.println(jedis.ping());
+	}
+
+}
