375695c5c8c616efd539ef1df39e43970d4ae4b0
hansol <oksktank@gmail.com>
2012-11-05 21:04:12 +0900

    API 완료

--- src/main/java/sla/controller/ApiController.java
+++ src/main/java/sla/controller/ApiController.java
@@ -13,9 +13,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import sla.model.KeyCount;
 import sla.model.Result;
 import sla.model.ShortUserInfoWithCount;
+import sla.model.UserInfo;
 import sla.service.AnalyzeService;
+import sla.util.DateUtil;
 
 @Controller
 @RequestMapping("api")
@@ -23,7 +26,16 @@ public class ApiController {
 	
 	@Autowired
 	AnalyzeService analyzeService;
-	
+	public String ret(Model model,Object object,boolean list) throws JsonGenerationException, JsonMappingException, IOException{
+		ObjectMapper om=new ObjectMapper();
+		if(list){
+			model.addAttribute("result",om.writeValueAsString(new Result(object)));
+		}else{
+			model.addAttribute("result",om.writeValueAsString(object));
+		}
+		return "result";
+		
+	}
 	@RequestMapping("countRecord")
 	public String getCountRecordByUser(Model model,@RequestParam String shortUrl,@RequestParam(required=false) Integer startPeriod,@RequestParam(required=false) Integer endPeriod) throws SQLException, JsonGenerationException, JsonMappingException, IOException{
 		
@@ -38,14 +50,48 @@ public class ApiController {
 		return ret(model,countRecord,true);
 	}
 	
-	public String ret(Model model,Object object,boolean list) throws JsonGenerationException, JsonMappingException, IOException{
-		ObjectMapper om=new ObjectMapper();
-		if(list){
-			model.addAttribute("result",om.writeValueAsString(new Result(object)));
-		}else{
-			model.addAttribute("result",om.writeValueAsString(object));
+	@RequestMapping("countSumByPeriod")
+	public String getCountSumByPeriod(Model model,@RequestParam String shortUrl,Integer endPeriod,
+			@RequestParam(required=false,defaultValue="10")Integer getCount,@RequestParam(required=false,defaultValue="0") Integer gubun,@RequestParam(required=false,defaultValue="true") boolean all) throws SQLException, JsonGenerationException, JsonMappingException, IOException {
+		if(endPeriod==null){
+			endPeriod=Integer.valueOf(DateUtil.getToday("YYYYMMDDHH"));
 		}
-		return "result";
-		
+		List<KeyCount> countSumByPeriod=analyzeService.getCountSumByPeriod(shortUrl, endPeriod, getCount, gubun, all);
+		return ret(model,countSumByPeriod,true);
 	}
+	
+	@RequestMapping("accumulatedCountSum")
+	public String getAccumulatedCountSumByPeriod(Model model,@RequestParam String shortUrl,Integer endPeriod,
+			@RequestParam(required=false,defaultValue="10")Integer getCount,@RequestParam(required=false,defaultValue="0") Integer gubun,@RequestParam(required=false,defaultValue="true") boolean all) throws SQLException, JsonGenerationException, JsonMappingException, IOException {
+		if(endPeriod==null){
+			endPeriod=Integer.valueOf(DateUtil.getToday("YYYYMMDDHH"));
+		}
+		List<KeyCount> accumulatedCountSum=analyzeService.getAccumulatedCountSumByPeriod(shortUrl, endPeriod, getCount, gubun, all);
+		return ret(model,accumulatedCountSum,true);
+	}
+	@RequestMapping("sharerInfo")
+	public String getUserInfoWithShortUrl(Model model, @RequestParam String shortUrl) throws JsonGenerationException, JsonMappingException, IOException{
+		UserInfo sharerInfo=analyzeService.getUserInfoWithShortUrl(shortUrl);
+		return ret(model,sharerInfo,false);
+	}
+	
+	@RequestMapping("genderDistribution")
+	public String getUserGenderDistribution(Model model,@RequestParam String shortUrl,@RequestParam(required=false,defaultValue="true") boolean all) throws JsonGenerationException, JsonMappingException, IOException, SQLException{
+		List<KeyCount> genderDist=analyzeService.getUserGenderDistribution(shortUrl, all);
+		return ret(model,genderDist,true);
+	}
+	
+	@RequestMapping("osDistribution")
+	public String getOsDistribution(Model model,@RequestParam String shortUrl,@RequestParam(required=false,defaultValue="true") boolean all) throws JsonGenerationException, JsonMappingException, IOException, SQLException{
+		List<KeyCount> osDist=analyzeService.getOperationSystemDistribution(shortUrl, all);
+		return ret(model,osDist,true);
+	}
+	
+	@RequestMapping("browserDistribution")
+	public String getBrowserDistribution(Model model,@RequestParam String shortUrl,@RequestParam(required=false,defaultValue="true") boolean all) throws JsonGenerationException, JsonMappingException, IOException, SQLException{
+		List<KeyCount> browserDist=analyzeService.getBrowserDistribution(shortUrl, all);
+		return ret(model,browserDist,true);
+	}
+	
+	
 }
--- src/main/webapp/WEB-INF/view/func/analyze.jsp
+++ src/main/webapp/WEB-INF/view/func/analyze.jsp
@@ -19,12 +19,12 @@
 	<body>
 	
 		분석 결과 보기<br/><br/>
-		공유자 : ${sharer}<br/><br/>
-		공유자 방문 유입 순위: ${countRecord }<br/><br/>
-		성별 분포: ${genderDistribution }<br/><br/>
-		OS 분포: ${operationSystemDistribution }<br/><br/>
-		브라우저 분포: ${browserDistribution }<br/><br/>
-		시간별 방문자 수 : ${countSum }<br/><br/>
-		시간별 누적 방문자 수 : ${accumulatedCountSum }
+		공유자 :/SLA/api/sharerInfo?shortUrl=IP<br/> ${sharer}<br/><br/>
+		공유자 방문 유입 순위: /SLA/api/countRecord?shortUrl=IP<br/> ${countRecord }<br/><br/>
+		성별 분포: /SLA/api/genderDistribution?shortUrl=IP<br/>${genderDistribution }<br/><br/>
+		OS 분포: /SLA/api/osDistribution?shortUrl=IP<br/>${operationSystemDistribution }<br/><br/>
+		브라우저 분포: /SLA/api/browserDistribution?shortUrl=IP<br/>${browserDistribution }<br/><br/>
+		시간별 방문자 수 :/SLA/api/countSumByPeriod?shortUrl=IP<br/> ${countSum }<br/><br/>
+		시간별 누적 방문자 수 :/SLA/api/accumulatedCountSum?shortUrl=IP<br/> ${accumulatedCountSum }
 	</body>
 </html>
