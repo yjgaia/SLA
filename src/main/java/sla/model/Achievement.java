95e4b1dffa407fe47452d50452f6623141d25e5d
hansol <oksktank@gmail.com>
2012-11-12 03:49:32 +0900

    Achievement 모델 생성

---
+++ src/main/java/sla/model/Achievement.java
@@ -0,0 +1,15 @@
+package sla.model;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Achievement {
+	String name;
+	int score;
+	String category;
+	String description;
+}
---
+++ src/main/java/sla/model/UserAchieve.java
@@ -0,0 +1,23 @@
+package sla.model;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class UserAchieve {
+	@ManyToOne
+	@JoinColumn(name = "userId", nullable = false)
+	private UserInfo userInfo;
+	
+	@ManyToOne
+	@JoinColumn(name = "achievementId", nullable = false)
+	private Achievement acheivement;
+
+
+}
