826c62c5a674edd72d8856c54c521e0aa84bc8c3
hansol <oksktank@gmail.com>
2012-11-18 07:38:26 +0900

    쿠키로 사용자 구별,저장

---
+++ src/main/java/sla/model/VisitDetail.java
@@ -0,0 +1,35 @@
+package sla.model;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class VisitDetail {
+	
+	@ManyToOne
+	@JoinColumn(name = "shortUrlId", nullable = false)
+	private ShortUrl shortUrl;
+	
+	private String cookieId;
+	private Long savedUserId;
+	private int visitCount;	
+	public void increaseVisitCount(){
+		visitCount++;
+	}
+	public static boolean existsVisitDetailByShortUrlAndCookieId(long shortUrlId,String cookieId){
+		return entityManager()
+				.createQuery("SELECT count(o) FROM VisitDetail o WHERE o.cookieId = :cookieId AND o.shortUrl.id=:shortUrlId",Long.class)
+				.setParameter("cookieId",cookieId).setParameter("shortUrlId", shortUrlId).getSingleResult()>0l;
+	}
+	public static VisitDetail findVisitDetailByShortUrlAndCookieId(long shortUrlId,String cookieId){
+		return entityManager()
+		.createQuery("SELECT o FROM VisitDetail o WHERE o.cookieId = :cookieId AND o.shortUrl.id=:shortUrlId",VisitDetail.class)
+		.setParameter("cookieId",cookieId).setParameter("shortUrlId", shortUrlId).getSingleResult();
+	}
+}
